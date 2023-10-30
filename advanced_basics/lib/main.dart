f622f7fc4290f984d4d6f381a7c5fa1b24e24683
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-10-30 16:04:14 +0300

    create questions screen and navigate between screens

--- advanced_basics/lib/main.dart
+++ advanced_basics/lib/main.dart
@@ -1,29 +1,6 @@
-import 'package:advanced_basics/start_screen.dart';
+import 'package:advanced_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Quiz App",
-      home: Scaffold(
-          body: Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 78, 13, 151),
-                  Color.fromARGB(255, 107, 15, 168),
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              )),
-              child: const StartScreen())),
-    );
-  }
+  runApp(const Quiz());
 }
