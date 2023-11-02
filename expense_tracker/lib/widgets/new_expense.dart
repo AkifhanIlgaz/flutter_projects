535d36305f1b2e174dc939be802d0d1306b7d339
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-11-02 11:24:29 +0300

    add amount controller

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -9,9 +9,11 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
+    _amountController.dispose();
     _titleController.dispose();
     super.dispose();
   }
@@ -29,11 +31,28 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text("Title"),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: "\$ ",
+              label: Text("Amount"),
+            ),
+          ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {
+                  _titleController.clear();
+                  _amountController.clear();
+                },
+                child: const Text("Cancel"),
+              ),
+              const Spacer(),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: const Text(
                   "Save expense",
