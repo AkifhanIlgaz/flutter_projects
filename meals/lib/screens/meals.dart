f636a7a429e25742f1b88213086ee59ba4b70fde
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-11-10 15:42:00 +0300

    create meal detail screen and navigate to this screen when meal is tapped

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -12,11 +13,27 @@ class MealsScreen extends StatelessWidget {
   final List<Meal> meals;
   final String title;
 
+  void _selectMeal(BuildContext context, Meal selectedMeal) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: selectedMeal,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (context, index) => MealItem(meal: meals[index]),
+      itemBuilder: (context, index) => MealItem(
+        meal: meals[index],
+        onSelectMeal: () {
+          _selectMeal(context, meals[index]);
+        },
+      ),
     );
 
     if (meals.isEmpty) {
@@ -32,7 +49,7 @@ class MealsScreen extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             Text(
-              "Try selectin a different category",
+              "Try selecting a different category",
               style: Theme.of(context)
                   .textTheme
                   .bodyLarge!
