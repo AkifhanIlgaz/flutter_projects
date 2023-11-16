2e5cd2d6c0594277b7479b94998960681f1fd606
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-11-16 16:27:57 +0300

    delete items by swiping

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -10,8 +10,10 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.push(
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.push<GroceryItem>(
       context,
       MaterialPageRoute(
         builder: (context) {
@@ -19,6 +21,14 @@ class _GroceryListState extends State<GroceryList> {
         },
       ),
     );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   @override
@@ -33,23 +43,35 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) {
-          return ListTile(
-            title: Text(groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: groceryItems[index].category.color,
+      body: _groceryItems.isEmpty
+          ? const Center(
+              child: Text("No items added yet!"),
+            )
+          : ListView.builder(
+              itemCount: _groceryItems.length,
+              itemBuilder: (ctx, index) {
+                return Dismissible(
+                  key: ValueKey(_groceryItems[index].id),
+                  onDismissed: (direction) {
+                    setState(() {
+                      _groceryItems.removeAt(index);
+                    });
+                  },
+                  child: ListTile(
+                    title: Text(_groceryItems[index].name),
+                    leading: Container(
+                      width: 24,
+                      height: 24,
+                      color: _groceryItems[index].category.color,
+                    ),
+                    trailing: Text(
+                      _groceryItems[index].quantity.toString(),
+                      style: Theme.of(context).textTheme.labelLarge,
+                    ),
+                  ),
+                );
+              },
             ),
-            trailing: Text(
-              groceryItems[index].quantity.toString(),
-              style: Theme.of(context).textTheme.labelLarge,
-            ),
-          );
-        },
-      ),
     );
   }
 }
