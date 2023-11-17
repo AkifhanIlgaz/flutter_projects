a21a7e2ade573885dca7e18e4a87096a85b51594
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-11-17 11:42:55 +0300

    use firebase realtime database

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
+
+const baseUrl = "flutter-prep-54c35-default-rtdb.firebaseio.com";
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -19,13 +23,26 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.pop( 
-          context,
-          GroceryItem(
-              id: DateTime.now().toString(),
-              name: _enteredName,
-              quantity: _enteredQuantity,
-              category: _selectedCategory));
+
+      final url = Uri.https(baseUrl, "shopping-list.json");
+
+      http.post(
+        url,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: json.encode(
+          {
+            "name": _enteredName,
+            "quantity": _enteredQuantity,
+            "category": _selectedCategory.title
+          },
+        ),
+      );
+
+      print("add");
+
+      Navigator.pop(context);
     }
   }
 
