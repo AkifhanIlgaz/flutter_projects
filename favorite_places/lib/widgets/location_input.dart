d2c72c1b0deba232e29f16228fbef8bed95651ab
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-11-20 15:55:25 +0300

    create PlaceLocation class

--- favorite_places/lib/widgets/location_input.dart
+++ favorite_places/lib/widgets/location_input.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:favorite_places/maps_api_key.dart';
+import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
 import 'package:location/location.dart';
@@ -13,7 +14,7 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
+  PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
   void _getCurrentLocation() async {
@@ -48,6 +49,10 @@ class _LocationInputState extends State<LocationInput> {
     final lat = locationData.latitude;
     final lng = locationData.longitude;
 
+    if (lat == null || lng == null) {
+      return;
+    }
+
     final url = Uri.parse(
         "https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=$mapsAPIKEY");
     final response = await http.get(url);
@@ -55,6 +60,8 @@ class _LocationInputState extends State<LocationInput> {
     final address = resData["results"][0]["formatted_address"];
 
     setState(() {
+      _pickedLocation =
+          PlaceLocation(latitude: lat, longitude: lng, address: address);
       _isGettingLocation = false;
     });
   }
