a7107e06fbf868f1f4c7a792357a34b468a47053
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-11-21 11:57:26 +0300

    validate the user input

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -9,6 +9,19 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   var _isLogin = true;
+  final _formKey = GlobalKey<FormState>();
+  late String _enteredEmail;
+  late String _enteredPassword;
+
+  void _submit() {
+    final isValid = _formKey.currentState!.validate();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+      print(_enteredEmail);
+      print(_enteredPassword);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +48,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(16),
                     child: Form(
+                      key: _formKey,
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -45,6 +59,17 @@ class _AuthScreenState extends State<AuthScreen> {
                             keyboardType: TextInputType.emailAddress,
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains("@")) {
+                                return "Please enter a valid email address";
+                              }
+                              return null;
+                            },
+                            onSaved: (email) {
+                              _enteredEmail = email!;
+                            },
                           ),
                           TextFormField(
                             decoration: const InputDecoration(
@@ -52,10 +77,19 @@ class _AuthScreenState extends State<AuthScreen> {
                             ),
                             keyboardType: TextInputType.text,
                             obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.trim().length < 6) {
+                                return "Password must be at least 6 characters long.";
+                              }
+                              return null;
+                            },
+                            onSaved: (password) {
+                              _enteredPassword = password!;
+                            },
                           ),
                           const SizedBox(height: 12),
                           ElevatedButton(
-                            onPressed: () {},
+                            onPressed: _submit,
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Theme.of(context)
                                   .colorScheme
